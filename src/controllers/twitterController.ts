1bce0dbdf5201339bed2ed7e0b134a7469d7fd52
Eric Rodrigues <er601432@gmail.com>
2021-12-21 11:43:24 -0300

    feat: adicionando editorconfig e demais configurações de lint

--- src/controllers/twitterController.ts
+++ src/controllers/twitterController.ts
@@ -1,27 +1,30 @@
-import {tweet} from '../connection/twitterConnection'
+import { tweet } from '../connection/twitterConnection';
 
-function retweet(event){
-  const {retweeted_status, id_str, screen_name, is_quote_status} = event;
-  const {name} = event.user;
+function retweet(event) {
+  const { retweeted_status, id_str, screen_name, is_quote_status } = event;
+  const { name } = event.user;
 
-  if(!retweeted_status && !is_quote_status){ 
-    tweet.post(`statuses/retweet/${id_str}`, err => { 
-      if(err){
-        console.log("Erro no retweet: " + err)
-      }else {
-        console.log("RETWEETADO: ", `https://twitter.com/${name}/status/${id_str}`)
+  if (!retweeted_status && !is_quote_status) {
+    tweet.post(`statuses/retweet/${id_str}`, err => {
+      if (err) {
+        console.log('Erro no retweet: ' + err);
+      } else {
+        console.log(
+          'RETWEETADO: ',
+          `https://twitter.com/${name}/status/${id_str}`,
+        );
       }
-    }) 
-    } else {
-       return
-     }
-
+    });
+  } else {
+    return;
+  }
 }
 
-function like(){
- return 0
+function like() {
+  return 0;
 }
 
 export default {
-  retweet, like
-}
+  retweet,
+  like,
+};
--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,7 @@
-import { tweet } from "./connection/twitterConnection"
-import twitterController from "./controllers/twitterController"
+import { tweet } from './connection/twitterConnection';
+import twitterController from './controllers/twitterController';
 
-const stream = tweet.stream('statuses/filter', {track: 'egirl'}) 
+const stream = tweet.stream('statuses/filter', { track: 'egirl' });
 
-stream.on('data', twitterController.retweet) 
-stream.on('error', err => console.log("Erro: "+ err)) 
-
+stream.on('data', twitterController.retweet);
+stream.on('error', err => console.log('Erro: ' + err));
